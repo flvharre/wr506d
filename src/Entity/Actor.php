b588e560f7724c735f1a93f8a9acb01467a85f2f
Github MMI3 <github@mmi-troyes.fr>
2025-09-15 12:21:01 +0000

    Feat: Added api platform

--- src/Entity/Actor.php
+++ src/Entity/Actor.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\ActorRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -10,6 +11,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ActorRepository::class)]
 #[ORM\HasLifecycleCallbacks]
+#[ApiResource]
 class Actor
 {
     #[ORM\Id]
