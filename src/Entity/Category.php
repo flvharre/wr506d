8ec470e5729267861171795dcce2d7caacc7da66
flvharre <harreflavie@gmail.com>
2025-10-01 07:47:21 +0000

    Feat: Added asserts to Entities + new Director Entity

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -7,6 +7,8 @@ use App\Repository\CategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
 #[ORM\HasLifecycleCallbacks]
@@ -19,6 +21,7 @@ class Category
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: "Le nom de la catégorie est obligatoire.")]
     private ?string $name = null;
 
     /**
