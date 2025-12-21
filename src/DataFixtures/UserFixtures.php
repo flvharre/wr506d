38a32569f7321cb5cb282f470193915e5e7e8830
flvharre <harreflavie@gmail.com>
2025-12-21 07:04:49 +0000

    feat: added fixture to create admin user in db for testing

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        // Créer le compte admin
+        $admin = new User();
+        $admin->setUsername('admin');
+        $admin->setEmail('admin@admin.com');
+        $admin->setRoles(['ROLE_ADMIN']);
+
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $admin,
+            'admin123'
+        );
+        $admin->setPassword($hashedPassword);
+
+        $manager->persist($admin);
+        $manager->flush();
+    }
+}
